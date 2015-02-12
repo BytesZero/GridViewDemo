425ddb0f2c8b3eba3259ddbeca57e937491f0d94
zhengsl <zhengsl@tongxinyiliao.com>
2015-02-12 11:03:42 +0800

    cube图片下载完成

--- app/src/main/java/com/zhengsonglan/recipesdaquan/MainActivity.java
+++ app/src/main/java/com/zhengsonglan/recipesdaquan/MainActivity.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import in.srain.cube.image.*;
+
 /**
  * 程序主界面
  */
 public class MainActivity extends BaseActivity {
     private TextView tv_tb_title;
+    private CubeImageView iv_img;
+
+
+    ImageLoader imageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +28,24 @@ public class MainActivity extends BaseActivity {
     /**
      * 初始化视图
      */
-    private void initView(){
-        tv_tb_title= (TextView) findViewById(R.id.unit_topbar_tv_title);
+    private void initView() {
+        tv_tb_title = (TextView) findViewById(R.id.unit_topbar_tv_title);
         tv_tb_title.setText("菜谱大全");
+        iv_img = (CubeImageView) findViewById(R.id.main_iv_img);
     }
 
     /**
      * 初始化数据
      */
-    private void initData(){
-
+    private void initData() {
+        imageLoader = ImageLoaderFactory.create(this);
+        iv_img.loadImage(imageLoader, "http://img5.imgtn.bdimg.com/it/u=1482866412,2033620421&fm=23&gp=0.jpg");
     }
 
     /**
      * 初始化时间
      */
-    private void initEvent(){
+    private void initEvent() {
         tv_tb_title.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
